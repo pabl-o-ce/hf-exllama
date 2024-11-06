5687d66b9dec108f12574dae95cba6818b7b4f7b
pabl-o-ce <pabloce@poscye.com>
2024-11-06 11:32:29 -0500

    fix: image adding allowed_path

--- app.py
+++ app.py
@@ -276,4 +276,4 @@ demo = gr.ChatInterface(
 )
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(allowed_paths=["https://huggingface.co/spaces/pabloce/llama-cpp-agent/resolve/main/llama.jpg"])
